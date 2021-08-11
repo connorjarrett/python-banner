e086b89d7a9ff413c7c3effd24bfba6f18590387
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2021-08-11 21:25:38 +0100

    Created module
    Added base for letter "A" but for some reason depending on the width some of it will not show at all

---
+++ module.py
@@ -0,0 +1,36 @@
+a_table = [
+1,1,
+1,1,
+1,1,
+1,1,
+1,1
+]
+
+tables = [a_table]
+letter_width = 4 # Don't change this, this is how many characters are in a line before creating a new one
+
+def printLetter(letter_position, blank_symbol, filled_symbol):
+	table = tables[letter_position]
+	width_counter = 0 # Don't change this, this counts how wide a letter is so far
+	line_out = ""
+	print(len(table))
+	for y in table:
+		if width_counter < letter_width:
+			if y == 1:
+				line_out = line_out + filled_symbol
+			else:
+				line_out = line_out + blank_symbol
+			width_counter = width_counter + 1
+		else:
+			print(line_out)
+			line_out = ""
+			width_counter = 0
+		
+def createBanner(content, blank_symbol, filled_symbol):
+	for x in content:
+		x = x.lower()
+
+		if x == "a":
+			printLetter(0, blank_symbol, filled_symbol)
+		
+createBanner("A",".", "#")

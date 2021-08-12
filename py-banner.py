e42bcc2e222ab9a9bad154d469ff8abfab81acec
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2021-08-12 14:03:21 +0100

    Fixed end of line error
    Needed to end the table for each letter with `None`, This is likely due to the use of `<` rather than `<=` which would not work for an unknown reason

--- py-banner.py
+++ py-banner.py
@@ -1,13 +1,14 @@
 a_table = [
-1,1,
-1,1,
-1,1,
-1,1,
-1,1
+1,1,0,
+1,1,0,
+1,1,0,
+1,1,0,
+1,1,0,
+None
 ]
 
 tables = [a_table]
-letter_width = 4 # Don't change this, this is how many characters are in a line before creating a new one
+letter_width = 3 # Don't change this, this is how many characters are in a line before creating a new one
 
 def printLetter(letter_position, blank_symbol, filled_symbol):
 	table = tables[letter_position]
@@ -25,6 +26,11 @@ def printLetter(letter_position, blank_symbol, filled_symbol):
 			print(line_out)
 			line_out = ""
 			width_counter = 0
+			if y == 1:
+				line_out = line_out + filled_symbol
+			else:
+				line_out = line_out + blank_symbol
+			width_counter = width_counter + 1
 		
 def createBanner(content, blank_symbol, filled_symbol):
 	for x in content:

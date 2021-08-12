4e810af30ca4b7e6963a2b1d7725edf40968a798
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2021-08-12 17:02:39 +0100

    Update py-banner.py
    Added up to "M" in letters

--- py-banner.py
+++ py-banner.py
@@ -51,12 +51,140 @@ d_table = [
 1,1,0,0,0,0,1,1,
 1,1,1,1,1,1,0,0,
 1,1,1,1,1,1,0,0,
-None,
+None
 ]
+
+e_table = [
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,1,1,1,1,0,0,
+1,1,1,1,1,1,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+None
+]
+
+f_table = [
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,1,1,1,1,0,0,
+1,1,1,1,1,1,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+None
+]
+
+g_table = [
+0,0,1,1,1,1,1,1,
+0,0,1,1,1,1,1,1,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,1,1,1,1,
+1,1,0,0,1,1,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+0,0,1,1,1,1,1,1,
+0,0,1,1,1,1,1,1,
+None
+]
+
+h_table = [
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+None
+]
+
+i_table = [
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+0,0,0,1,1,0,0,0,
+0,0,0,1,1,0,0,0,
+0,0,0,1,1,0,0,0,
+0,0,0,1,1,0,0,0,
+0,0,0,1,1,0,0,0,
+0,0,0,1,1,0,0,0,
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+None
+]
+
+j_table = [
+0,0,0,0,0,0,1,1,
+0,0,0,0,0,0,1,1,
+0,0,0,0,0,0,1,1,
+0,0,0,0,0,0,1,1,
+0,0,0,0,0,0,1,1,
+0,0,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+0,0,1,1,1,1,0,0,
+0,0,1,1,1,1,0,0,
+None
+]
+
+k_table = [
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,1,1,0,0,
+1,1,0,0,1,1,0,0,
+1,1,1,1,0,0,0,0,
+1,1,1,1,0,0,0,0,
+1,1,0,0,1,1,0,0,
+1,1,0,0,1,1,0,0,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+None
+]
+
+l_table = [
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,0,0,0,0,0,0,
+1,1,1,1,1,1,1,1,
+1,1,1,1,1,1,1,1,
+None
+]
+
+m_table = [
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,1,0,0,1,1,1,
+1,1,1,0,0,1,1,1,
+1,1,0,1,1,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+1,1,0,0,0,0,1,1,
+None
+]
+
+
 
 final_print = []
 
-tables = [a_table, b_table, c_table, d_table]
+tables = [a_table, b_table, c_table, d_table, e_table, f_table, g_table, h_table, i_table, j_table, k_table, l_table, m_table, n_table, o_table, p_table, q_table, r_table, s_table, t_table, u_table, v_table, w_table, x_table, y_table, z_table]
 letter_width = 8 # Don't change this, this is how many characters are in a line before creating a new one
 
 def outputPrint(table, out_len, blank_symbol):
@@ -72,7 +200,6 @@ def printLetter(letter_position, blank_symbol, filled_symbol):
 	table = tables[letter_position]
 	width_counter = 0 # Don't change this, this counts how wide a letter is so far
 	line_out = ""
-	print(len(table))
 	for y in table:
 		if width_counter < letter_width:
 			if y == 1:
@@ -89,7 +216,6 @@ def printLetter(letter_position, blank_symbol, filled_symbol):
 			else:
 				line_out = line_out + blank_symbol
 			width_counter = width_counter + 1
-	print("-------------")
 	
 	
 def createBanner(content, blank_symbol, filled_symbol):
@@ -103,6 +229,51 @@ def createBanner(content, blank_symbol, filled_symbol):
 			printLetter(2, blank_symbol, filled_symbol)
 		elif x == "d":
 			printLetter(3, blank_symbol, filled_symbol)
+		elif x == "e":
+			printLetter(4, blank_symbol, filled_symbol)
+		elif x == "f":
+			printLetter(5, blank_symbol, filled_symbol)
+		elif x == "g":
+			printLetter(6, blank_symbol, filled_symbol)
+		elif x == "h":
+			printLetter(7, blank_symbol, filled_symbol)
+		elif x == "i":
+			printLetter(8, blank_symbol, filled_symbol)
+		elif x == "j":
+			printLetter(9, blank_symbol, filled_symbol)
+		elif x == "k":
+			printLetter(10, blank_symbol, filled_symbol)
+		elif x == "l":
+			printLetter(11, blank_symbol, filled_symbol)
+		elif x == "m":
+			printLetter(12, blank_symbol, filled_symbol)
+		elif x == "n":
+			printLetter(13, blank_symbol, filled_symbol)
+		elif x == "o":
+			printLetter(14, blank_symbol, filled_symbol)
+		elif x == "p":
+			printLetter(15, blank_symbol, filled_symbol)
+		elif x == "q":
+			printLetter(16, blank_symbol, filled_symbol)
+		elif x == "r":
+			printLetter(17, blank_symbol, filled_symbol)
+		elif x == "s":
+			printLetter(18, blank_symbol, filled_symbol)
+		elif x == "t":
+			printLetter(19, blank_symbol, filled_symbol)
+		elif x == "u":
+			printLetter(20, blank_symbol, filled_symbol)
+		elif x == "v":
+			printLetter(21, blank_symbol, filled_symbol)
+		elif x == "w":
+			printLetter(22, blank_symbol, filled_symbol)
+		elif x == "x":
+			printLetter(23, blank_symbol, filled_symbol)
+		elif x == "y":
+			printLetter(24, blank_symbol, filled_symbol)
+		elif x == "z":
+			printLetter(25, blank_symbol, filled_symbol)
+		
 	outputPrint(final_print, len(content), blank_symbol)
 		
-createBanner("ABCD",".", "#")
+createBanner("ABCDEFGHIJKLMNOPQRSTUVWXYZ",".", "#")
